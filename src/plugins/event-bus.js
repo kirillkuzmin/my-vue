a8ea280d522626e97b32373373ff8e09e752c63f
kirillkuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2017-11-29 12:54:34 +0300

    Create event-bus.js

---
+++ src/plugins/event-bus.js
@@ -0,0 +1,31 @@
+class EventBus {
+  constructor() {
+    this.vue = new Vue();
+  }
+
+  fire(event, data = null) {
+    this.vue.$emit(event, data);
+  }
+
+  listen(event, callback) {
+    this.vue.$on(event, callback);
+  }
+
+  destroy() {
+    this.vue.$off();
+  }
+}
+
+const bus = new EventBus;
+
+export default {
+  install(Vue) {
+    Object.defineProperties(Vue.prototype, {
+      $bus: {
+        get() {
+          return bus;
+        },
+      },
+    });
+  },
+};
