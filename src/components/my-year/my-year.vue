3fda1846adccf795ee7940b415c816662f5fee09
kirillkuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2017-11-29 12:56:24 +0300

    Create my-year.vue

---
+++ src/components/my-year/my-year.vue
@@ -0,0 +1,69 @@
+<template>
+  <select :id="id" :name="name" v-model="selected">
+    <option value="-1" style="font-style: italic;" v-if="all">- Все -</option>
+    <option
+      v-for="(year, y) in yearRange"
+      :value="year"
+      v-text="year"
+    >
+    </option>
+  </select>
+</template>
+
+<script>
+  export default {
+    props: {
+      all: {
+        type: Boolean,
+        default: false,
+      },
+
+      endYear: {
+        type: Number,
+        default: new Date().getFullYear() + 2,
+      },
+
+      id: String,
+
+      name: String,
+
+      startYear: {
+        type: Number,
+        default: 2006,
+      },
+
+      value: [String, Number],
+    },
+
+    data() {
+      return {};
+    },
+
+    computed: {
+      selected: {
+        get() {
+          return this.value;
+        },
+
+        set(val) {
+          this.$emit('input', val);
+        },
+      },
+
+      yearRange() {
+        let range = [];
+
+        for (let y = this.startYear; y <= this.endYear; y++) {
+          range.push(y);
+        }
+
+        return range;
+      },
+    },
+
+    methods: {},
+  };
+</script>
+
+<style lang="less" scoped>
+</style>
