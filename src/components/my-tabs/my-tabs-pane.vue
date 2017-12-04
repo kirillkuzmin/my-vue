d2e550a4a95157a4fa8312885058865fb7b5c313
Kirill Kuzmin <Kirill.Kuzmin@ru.amadeus.com>
2017-12-04 16:33:19 +0300

    Initial commit

---
+++ src/components/my-tabs/my-tabs-pane.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="my-tabs__pane" :id="id" v-show="isActive">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+
+      id: {
+        type: String,
+        default () {
+          return Math.random().toString(36).slice(2);
+        },
+      },
+
+      selected: {
+        type: Boolean,
+        default: false,
+      },
+
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+
+    data () {
+      return {
+        isActive: false,
+        validators: [],
+      };
+    },
+
+    created () {
+      this.$bus.listen('my-tabs:add-validator', data => {
+        if (data.tabId === this.id) {
+          this.validators.push(data.validator);
+        }
+      });
+    },
+
+    mounted () {
+      this.isActive = this.selected;
+    },
+
+    computed: {
+      isDisabled () {
+        return this.disabled;
+      },
+
+      hasErrors () {
+        return this.validators.filter(validator => {
+          return validator.hasErrors();
+        }).length > 0;
+      },
+    },
+
+    methods: {},
+  };
+</script>
