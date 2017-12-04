86d665cb3d97eee9dda197b4ca244129d2ddacc1
Kirill Kuzmin <Kirill.Kuzmin@ru.amadeus.com>
2017-12-04 15:48:08 +0300

    Initial commit

---
+++ src/components/my-tabs/my-tabs-fixed.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="my-tabs">
+    <ul>
+      <li
+        v-for="(tab, index) in myTabs.tabs"
+        :class="getTabClass(tab)"
+        v-text="tab.title"
+        @click="myTabs.selectTab(tab, index)"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {},
+
+    data () {
+      return {
+        myTabs: {},
+      };
+    },
+
+    created () {
+      this.$bus.listen('my-tabs-fixed:init', myTabs => {
+        this.myTabs = myTabs;
+      });
+    },
+
+    methods: {
+      getTabClass (tab) {
+        return [
+          'my-tabs__tab',
+          {
+            'my-tabs__tab--active': tab.isActive,
+            'my-tabs__tab--disabled': tab.isDisabled,
+            'my-tabs__tab--has-errors': tab.hasErrors,
+          },
+        ];
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+</style>
