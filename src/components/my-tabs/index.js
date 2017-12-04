3c2940905ea2919f9ee50335b314ea3284878d29
Kirill Kuzmin <Kirill.Kuzmin@ru.amadeus.com>
2017-12-04 16:33:04 +0300

    Refactoring

--- src/components/my-tabs/index.js
+++ src/components/my-tabs/index.js
@@ -1,7 +1,9 @@
 import MyTabs from './my-tabs.vue';
 import MyTabsFixed from './my-tabs-fixed.vue';
+import MyTabsPane from './my-tabs-pane.vue';
 
 export {
   MyTabs,
   MyTabsFixed,
+  MyTabsPane,
 };
