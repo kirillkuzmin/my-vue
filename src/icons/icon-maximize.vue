0c22434e0e437d3bf109bcff4a9355ee48af4bd5
Kirill Kuzmin <rill7@yandex.ru>
2017-12-20 15:21:56 +0300

    initial commit

---
+++ src/icons/icon-maximize.vue
@@ -0,0 +1,48 @@
+<template>
+  <my-icon
+    :icon="icon"
+    :size="size"
+  />
+</template>
+
+<script>
+  import MyIcon from 'components/my-icon-new.vue';
+  import s16 from 'images/icons/16/maximize.svg';
+
+  export default {
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+
+      size: {
+        type: [Number, String],
+        default: 16,
+      },
+    },
+
+    data () {
+      return {
+        icons: {
+          '16': s16,
+        },
+      };
+    },
+
+    components: {
+      'my-icon': MyIcon,
+    },
+
+    computed: {
+      icon () {
+        const name = this.size + (this.disabled ? '_disabled' : '');
+
+        return this.icons[name];
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+</style>
