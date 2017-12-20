d70481ad179397753fd07a7215593ee8ba0c545e
Kirill Kuzmin <rill7@yandex.ru>
2017-12-20 15:23:43 +0300

    my-icon name fix

--- src/icons/icon-close.vue
+++ src/icons/icon-close.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-  import MyIcon from 'components/my-icon-new.vue';
+  import MyIcon from 'components/my-icon.vue';
   import s16 from 'images/icons/16/close.svg';
 
   export default {
