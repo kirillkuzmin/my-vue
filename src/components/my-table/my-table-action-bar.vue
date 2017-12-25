6836a428a5733223f185a42ea92b39707a6d77d4
Kirill Kuzmin <rill7@yandex.ru>
2017-12-25 09:59:51 +0300

    v1.0.8

---
+++ src/components/my-table/my-table-action-bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {},
+
+    data () {
+      return {};
+    },
+
+    components: {},
+
+    directives: {},
+
+    computed: {},
+
+    methods: {},
+  };
+</script>
+
+<style lang="less" scoped>
+
+</style>
