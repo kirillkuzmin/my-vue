21ef26a2cea803c6175d81fe5e33351019bd2aa6
Kirill Kuzmin <rill7@yandex.ru>
2017-12-29 13:11:31 +0300

    initial commit

---
+++ src/icons/index.js
@@ -0,0 +1,13 @@
+import IconClose from './icon-close.vue';
+import IconMaximize from './icon-maximize.vue';
+import IconMinimize from './icon-minimize.vue';
+import IconOpacity from './icon-opacity.vue';
+import IconSettings from './icon-settings.vue';
+
+export {
+  IconClose,
+  IconMaximize,
+  IconMinimize,
+  IconOpacity,
+  IconSettings,
+};
