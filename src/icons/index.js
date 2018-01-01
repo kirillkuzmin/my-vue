66284cd6a8d9921665d7de1f259ea7f2775b1a34
Kirill Kuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2018-01-01 13:07:03 +0300

    added icon-datepicker

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,6 @@
 import IconAdd from './icon-add.vue';
 import IconClose from './icon-close.vue';
+import IconDatepicker from './icon-datepicker.vue';
 import IconMaximize from './icon-maximize.vue';
 import IconMinimize from './icon-minimize.vue';
 import IconOpacity from './icon-opacity.vue';
@@ -9,6 +10,7 @@ import IconSettings from './icon-settings.vue';
 export {
   IconAdd,
   IconClose,
+  IconDatepicker,
   IconMaximize,
   IconMinimize,
   IconOpacity,
