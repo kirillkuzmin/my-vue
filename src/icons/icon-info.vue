ee2959e1c1edbd69eb0ab1203df9506ac46ef881
Kirill Kuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2018-01-04 23:26:06 +0300

    initial commit

---
+++ src/icons/icon-info.vue
@@ -0,0 +1,31 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    props: {
+      color: {
+        type: String,
+        default: '#689f38',
+      },
+    },
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M0 0h24v24H0z',
+              fill: 'none',
+            },
+            {
+              d: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
