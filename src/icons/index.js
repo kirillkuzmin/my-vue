ba2e9e11f84bac2ef9669b2f9282c87c147e4d9c
Kirill Kuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2018-01-05 14:15:10 +0300

    added several icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,19 +1,28 @@
 import IconAdd from './icon-add.vue';
+import IconArrowLeft from './icon-arrow-left.vue';
+import IconArrowRight from './icon-arrow-right.vue';
 import IconClose from './icon-close.vue';
 import IconDatepicker from './icon-datepicker.vue';
+import IconError from './icon-error.vue';
+import IconInfo from './icon-info.vue';
 import IconMaximize from './icon-maximize.vue';
 import IconMinimize from './icon-minimize.vue';
 import IconOpacity from './icon-opacity.vue';
 import IconRemove from './icon-remove.vue';
 import IconSettings from './icon-settings.vue';
+import IconWarning from './icon-warning.vue';
 
 export {
   IconAdd,
+  IconArrowLeft,
+  IconArrowRight,
   IconClose,
   IconDatepicker,
+  IconError,
   IconMaximize,
   IconMinimize,
   IconOpacity,
   IconRemove,
   IconSettings,
+  IconWarning,
 };
