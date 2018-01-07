b6ceb971b96435509c2cf7f393cb962708dc67d3
Kirill Kuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2018-01-07 20:24:22 +0300

    initial commit

---
+++ src/components/my-menu/index.js
@@ -0,0 +1,9 @@
+import MyMenu from './my-menu.vue';
+import MyMenuItem from './my-menu-item.vue';
+import MyMenuSection from './my-menu-section.vue';
+
+export {
+  MyMenu,
+  MyMenuItem,
+  MyMenuSection,
+};
