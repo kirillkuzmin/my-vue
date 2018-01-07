1c9d2495a2efef94dbdf3c081483b4b50c63b733
Kirill Kuzmin <34069977+kirillkuzmin@users.noreply.github.com>
2018-01-08 02:51:49 +0300

    initial commit

---
+++ src/components/my-menu/my-menu-section.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="my-menu-section">
+    <div class="my-menu-section__header" v-text="header"></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      header: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+  .my-menu-section {
+    &__header {
+      color: #000;
+      margin-left: 2px;
+      margin-right: 2px;
+      padding: 5px 10px 5px 5px;
+      vertical-align: middle;
+    }
+
+    & > .my-menu-item {
+      text-indent: 20px;
+    }
+  }
+</style>
