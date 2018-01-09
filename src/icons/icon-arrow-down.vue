4d2f6179d6f5984db8d6dd3522216e62453404af
Kirill Kuzmin <rill7@yandex.ru>
2018-01-09 13:18:41 +0300

    added icons for my-select

---
+++ src/icons/icon-arrow-down.vue
@@ -0,0 +1,24 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M7 10l5 5 5-5z',
+            },
+            {
+              d: 'M0 0h24v24H0z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
---
+++ src/icons/icon-clear.vue
@@ -0,0 +1,24 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z',
+            },
+            {
+              d: 'M0 0h24v24H0z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
