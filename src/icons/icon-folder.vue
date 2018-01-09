eb94239e2972e314b26faed3559f52bf41ec41de
Kirill Kuzmin <rill7@yandex.ru>
2018-01-09 11:43:26 +0300

    added icons for my-tree

---
+++ src/icons/icon-folder.vue
@@ -0,0 +1,31 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    props: {
+      color: {
+        type: String,
+        default: '#005eb8',
+      },
+    },
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z',
+            },
+            {
+              d: 'M0 0h24v24H0z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
