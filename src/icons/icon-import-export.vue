3ae201bafa53eb7ff8da062b09eecbfe23d3a4ba
Kirill Kuzmin <rill7@yandex.ru>
2018-01-12 15:11:34 +0300

    initial commit

---
+++ src/icons/icon-import-export.vue
@@ -0,0 +1,31 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    props: {
+      color: {
+        type: String,
+        default: '#005eb8',
+      },
+    },
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M9 3L5 6.99h3V14h2V6.99h3L9 3zm7 14.01V10h-2v7.01h-3L15 21l4-3.99h-3z',
+            },
+            {
+              d: 'M0 0h24v24H0z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
