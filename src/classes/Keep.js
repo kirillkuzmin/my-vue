23a96e4e84f8149cb736738f8913f5e900c9a640
Kirill Kuzmin <rill7@yandex.ru>
2018-01-12 15:11:45 +0300

    initial commit

---
+++ src/classes/Keep.js
@@ -0,0 +1,22 @@
+class Keep {
+  constructor () {
+  }
+
+  static get (path = '') {
+    try {
+      return JSON.parse(localStorage.getItem(path));
+    } catch (e) {
+      return localStorage.getItem(path);
+    }
+  }
+
+  static set (path, value) {
+    if (_.isObject(value)) {
+      value = JSON.stringify(value);
+    }
+
+    localStorage.setItem(path, value);
+  }
+}
+
+export default Keep;
