e7ba9e1fb7bc1ecc9a8de8f93603f1570fe4061f
Kirill Kuzmin <rill7@yandex.ru>
2018-01-19 18:40:44 +0300

    update

--- src/mixins/locale.js
+++ src/mixins/locale.js
@@ -1,9 +1,10 @@
+import deepFind from 'utils/deep-find';
 import i18n from 'locale';
 
 export default {
   methods: {
-    t (key = '') {
-      return i18n[this.$my.lang][key] || '';
+    t (path = '') {
+      return deepFind(i18n[this.$my.lang], path) || '';
     },
   },
 };
