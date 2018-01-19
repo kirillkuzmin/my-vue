99ea87dfe54f89d61a61621ad3d4c4cc9de30f6e
Kirill Kuzmin <rill7@yandex.ru>
2018-01-19 18:40:09 +0300

    initial commit

---
+++ src/utils/deep-find.js
@@ -0,0 +1,14 @@
+export default function (object, path) {
+  let p = path.split('.');
+  let c = object;
+
+  for (let i = 0; i < p.length; ++i) {
+    if (c[p[i]] === undefined) {
+      break;
+    }
+
+    c = c[p[i]];
+  }
+
+  return c;
+}
