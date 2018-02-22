bb8a0093a49ac8bc8a79641770cc4b8ccb186f05
Kirill Kuzmin <rill7@yandex.ru>
2018-02-22 15:16:57 +0300

    added my-overlay helpers

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -2,4 +2,13 @@ export default {
   created () {
     this.$options.delimiters = ['${', '}'];
   },
+  methods: {
+    $_showOverlay (text, hash = 'global') {
+      this.$bus.fire('my-overlay:show', hash);
+    },
+
+    $_hideOverlay (text, hash = 'global') {
+      this.$bus.fire('my-overlay:hide', hash);
+    },
+  },
 };
