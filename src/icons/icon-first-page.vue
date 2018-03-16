c8008b1732a9154d4407691871dea9cd39a6bc3e
Kirill Kuzmin <rill7@yandex.ru>
2018-03-16 10:39:48 +0300

    added icon-first-page and icon-last-page

---
+++ src/icons/icon-first-page.vue
@@ -0,0 +1,33 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    name: 'icon-first-page',
+
+    props: {
+      color: {
+        type: String,
+        default: '#000',
+      },
+    },
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z',
+            },
+            {
+              d: 'M24 24H0V0h24v24z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
---
+++ src/icons/icon-last-page.vue
@@ -0,0 +1,33 @@
+<script>
+  import MyIcon from 'components/my-icon';
+
+  export default {
+    extends: MyIcon,
+
+    name: 'icon-last-page',
+
+    props: {
+      color: {
+        type: String,
+        default: '#000',
+      },
+    },
+
+    data () {
+      return {
+        icon: {
+          size: 24,
+          paths: [
+            {
+              d: 'M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z',
+            },
+            {
+              d: 'M0 0h24v24H0V0z',
+              fill: 'none',
+            },
+          ],
+        },
+      };
+    },
+  };
+</script>
