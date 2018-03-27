b16f420667a3f02aa76296ef82fd66a3245609b6
Kirill Kuzmin <rill7@yandex.ru>
2018-03-27 13:36:54 +0300

    my-alert update

--- src/components/my-alert/my-alert.vue
+++ src/components/my-alert/my-alert.vue
@@ -1,17 +1,24 @@
 <template>
-  <div :class="className">
+  <div :class="className" v-if="show">
     <component
       :is="iconComponent"
       class="my-alert__icon"
-      v-if="icon"
+      v-if="typeIcon"
     />
     <div class="my-alert__content">
       <slot></slot>
     </div>
+    <icon-close
+      :color="closeColor"
+      class="my-alert__close"
+      v-if="hideIcon"
+      @click="hide"
+    />
   </div>
 </template>
 
 <script>
+  import IconClose from 'icons/icon-close.vue';
   import IconError from 'icons/icon-error.vue';
   import IconInfo from 'icons/icon-info.vue';
   import IconWarning from 'icons/icon-warning.vue';
@@ -20,18 +27,30 @@
     name: 'my-alert',
 
     props: {
+      hideIcon: {
+        type: Boolean,
+        default: false,
+      },
+
+      typeIcon: {
+        type: Boolean,
+        default: true,
+      },
+
       type: {
         type: String,
         default: 'warning',
       },
+    },
 
-      icon: {
-        type: Boolean,
-        default: true,
-      },
+    data () {
+      return {
+        show: true,
+      };
     },
 
     components: {
+      IconClose,
       IconError,
       IconInfo,
       IconWarning,
@@ -42,7 +61,6 @@
         return [
           'my-alert',
           {
-            //'my-alert--no-icon': !this.icon,
             'my-alert--error': this.type === 'error',
             'my-alert--warning': this.type === 'warning',
             'my-alert--info': this.type === 'info',
@@ -50,8 +68,22 @@
         ];
       },
 
+      closeColor () {
+        return '#33691e';
+      },
+
+      closeClass () {
+        return `my-alert__close--${this.type}`;
+      },
+
       iconComponent () {
         return `icon-${this.type}`;
+      },
+    },
+
+    methods: {
+      hide () {
+        this.show = false;
       },
     },
   };
